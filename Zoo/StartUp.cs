9bc03f70dc219ef5d19a1d012d72b2e4a989b39b
Kiril Kirilov <kirilkirilov@abv.bg>
2021-02-26 22:07:45 +0200

    Add files via upload

---
+++ Zoo/StartUp.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Zoo
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            Mammal dog = new Mammal("Sharo");
+
+            Console.WriteLine(dog.Name);
+        }
+    }
+}
