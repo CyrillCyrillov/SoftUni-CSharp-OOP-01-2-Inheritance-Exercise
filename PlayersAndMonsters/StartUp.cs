8d8cf0dd8d02d5301a56226b8534736c54631b30
Kiril Kirilov <kirilkirilov@abv.bg>
2021-02-27 00:40:49 +0200

    Add files via upload

---
+++ PlayersAndMonsters/StartUp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace PlayersAndMonsters
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            SoulMaster test = new SoulMaster("John", 15);
+            Knight boom = new Knight("Phill", 10);
+
+            Console.WriteLine(test);
+            Console.WriteLine(boom);
+        }
+    }
+}
