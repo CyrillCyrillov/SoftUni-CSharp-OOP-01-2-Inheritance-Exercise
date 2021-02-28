1fbc263b324bb423d6f54aefdb1450f1ede93893
Kiril Kirilov <kirilkirilov@abv.bg>
2021-02-28 20:00:30 +0200

    Add files via upload

---
+++ Restaurant/StartUp.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Restaurant
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            Cake redVelvet = new Cake("Red Velvet");
+            Console.WriteLine($"{redVelvet.Name} - {redVelvet.Price} - {redVelvet.Grams} - {redVelvet.Calories}");
+        }
+    }
+}
